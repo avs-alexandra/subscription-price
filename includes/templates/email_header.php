58576ac2bdf449a6ae637849be70c5f5c491d783
Alexandra <109337175+avs-alexandra@users.noreply.github.com>
2025-05-15 19:07:39 +0700

    Create email_header.php

---
+++ includes/templates/email_header.php
@@ -0,0 +1,26 @@
+<div style="background-color:{background_color};margin:0;padding:70px 0 70px 0;width:100%">
+    <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+        <tbody>
+        <tr>
+            <td align="center" valign="top">
+                <div id="template_header_image"></div>
+                <table border="0" cellpadding="0" cellspacing="0" width="600"
+                       style="background-color:#ffffff;border:1px solid #dedede;border-radius:3px">
+                    <tbody>
+                    <tr>
+                        <td align="center" valign="top">
+                            <table border="0" cellpadding="0" cellspacing="0" width="600"
+                                   style="background-color:{woocommerce_email_base_color};border-bottom-style:none;border-bottom-width:0;border-radius:3px 3px 0 0;color:#ffffff;font-family:{font_family};font-weight:bold;line-height:100%;vertical-align:middle">
+                                <tbody>
+                                <tr>
+                                    <td id="header_wrapper"
+                                        style="display:block;padding:36px 48px 36px 48px">
+                                        <h1 style="color:#ffffff;font-family:{font_family};font-size:30px;font-weight:300;line-height:150%;margin:0;text-align:left">
+                                            {title}
+                                        </h1>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
