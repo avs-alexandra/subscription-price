48689a9add14e7a53eb05ab3e130f7492b3dc778
Alexandra <109337175+avs-alexandra@users.noreply.github.com>
2025-05-15 22:55:43 +0700

    Update class-subscription-settings.php

--- includes/class-subscription-settings.php
+++ includes/class-subscription-settings.php
@@ -1,7 +1,5 @@
 <?php
-if (!defined('ABSPATH')) {
-    exit; // Запрет прямого доступа
-}
+if (!defined('ABSPATH')) exit; // Запрет прямого доступа
 
 class Subscription_Settings {
     public function __construct() {
@@ -64,6 +62,8 @@ class Subscription_Settings {
             <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
                 <?php wp_nonce_field('save_subscription_settings', 'woocommerce_subscription_nonce'); ?>
                 <input type="hidden" name="action" value="save_subscription_settings">
+                <!-- Поля для настройки подписок -->
+                <h2><?php _e('Настройки подписок', 'subscription-price'); ?></h2>
                 <table class="form-table">
                     <?php for ($i = 1; $i <= 4; $i++): ?>
                         <tr>
@@ -137,6 +137,66 @@ class Subscription_Settings {
                         </td>
                     </tr>
                 </table>
+                 <!-- Поля для настройки писем -->
+                <h2><?php _e('Настройки писем', 'subscription-price'); ?></h2>
+                <table class="form-table">
+                    <tr>
+                        <th scope="row">
+                            <label for="reminder_email_subject"><?php _e('Тема письма (напоминание)', 'subscription-price'); ?></label>
+                        </th>
+                        <td>
+                            <input type="text" id="reminder_email_subject" name="reminder_email_subject" style="width: 70%;" value="<?php echo esc_attr(get_option('reminder_email_subject', __('Ваша подписка скоро завершится', 'subscription-price'))); ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="reminder_email_body"><?php _e('Текст письма (напоминание)', 'subscription-price'); ?></label>
+                        </th>
+                        <td>
+                            <?php
+                            $reminder_email_body = get_option('reminder_email_body', __('Здравствуйте, [user]! Ваша подписка закончится [end_date].', 'subscription-price'));
+                            wp_editor($reminder_email_body, 'reminder_email_body', [
+                                'textarea_name' => 'reminder_email_body',
+                                'textarea_rows' => 2,
+                                'media_buttons' => true,
+                            ]);
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="expired_email_subject"><?php _e('Тема письма (завершение)', 'subscription-price'); ?></label>
+                        </th>
+                        <td>
+                            <input type="text" id="expired_email_subject" name="expired_email_subject" style="width: 70%;" value="<?php echo esc_attr(get_option('expired_email_subject', __('Ваша подписка завершилась', 'subscription-price'))); ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="expired_email_body"><?php _e('Текст письма (завершение)', 'subscription-price'); ?></label>
+                        </th>
+                        <td>
+                            <?php
+                            $expired_email_body = get_option('expired_email_body', __('Здравствуйте, [user]! Ваша подписка завершилась.', 'subscription-price'));
+                            wp_editor($expired_email_body, 'expired_email_body', [
+                                'textarea_name' => 'expired_email_body',
+                                'textarea_rows' => 2,
+                                'media_buttons' => true,
+                            ]);
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="email_font"><?php _e('Шрифт письма', 'subscription-price'); ?></label>
+                        </th>
+                        <td>
+                            <input type="text" id="email_font" name="email_font" style="width: 50%;" value="<?php echo esc_attr(get_option('email_font', 'Arial, sans-serif')); ?>">
+                            <p class="description"><?php _e('Укажите шрифт для письма (например, Arial, sans-serif).', 'subscription-price'); ?></p>
+                        </td>
+                    </tr>
+                </table>
+                
                 <p class="submit">
                     <button type="submit" class="button-primary"><?php _e('Сохранить изменения', 'subscription-price'); ?></button>
                 </p>
@@ -145,55 +205,59 @@ class Subscription_Settings {
         <?php
     }
 
-  /**
- * Сохранение настроек
- */
-public function save_settings() {
-    if (!isset($_POST['woocommerce_subscription_nonce']) || !wp_verify_nonce($_POST['woocommerce_subscription_nonce'], 'save_subscription_settings')) {
-        return; // Проверяем nonce для безопасности
-    }
+    /**
+     * Сохранение настроек
+     */
+    public function save_settings() {
+        if (!isset($_POST['woocommerce_subscription_nonce']) || !wp_verify_nonce($_POST['woocommerce_subscription_nonce'], 'save_subscription_settings')) {
+            return; // Проверяем nonce для безопасности
+        }
+        // Сохраняем настройки писем
+        update_option('reminder_email_subject', sanitize_text_field($_POST['reminder_email_subject']));
+        update_option('reminder_email_body', wp_kses_post($_POST['reminder_email_body']));
+        update_option('expired_email_subject', sanitize_text_field($_POST['expired_email_subject']));
+        update_option('expired_email_body', wp_kses_post($_POST['expired_email_body']));
+        update_option('email_font', sanitize_text_field($_POST['email_font']));
 
-    // Сохраняем роли для активной и истекшей подписки
-    $role_active = sanitize_text_field($_POST['role_active'] ?? '');
-    $role_expired = sanitize_text_field($_POST['role_expired'] ?? '');
+        // Сохраняем роли для активной и истекшей подписки
+        $role_active = sanitize_text_field($_POST['role_active'] ?? '');
+        $role_expired = sanitize_text_field($_POST['role_expired'] ?? '');
 
-    // Проверяем, чтобы роли активной и истекшей подписки не совпадали
-    if ($role_active === $role_expired) {
-        wp_redirect(admin_url('admin.php?page=subscription-settings&message=error_roles'));
+        // Проверяем, чтобы роли активной и истекшей подписки не совпадали
+        if ($role_active === $role_expired) {
+            wp_redirect(admin_url('admin.php?page=subscription-settings&message=error_roles'));
+            exit;
+        }
+
+        update_option('subscription_role_active', $role_active);
+        update_option('subscription_role_expired', $role_expired);
+
+        // Формируем массив всех подписок
+        $all_subscriptions = [];
+        for ($i = 1; $i <= 4; $i++) {
+            $product_id = absint($_POST["product_{$i}"] ?? 0);
+
+            if ($product_id > 0) {
+                $all_subscriptions[] = [
+                    'product_id' => $product_id,
+                    'duration' => [
+                        'years' => absint($_POST["duration_{$i}_years"] ?? 0),
+                        'months' => absint($_POST["duration_{$i}_months"] ?? 0),
+                        'days' => absint($_POST["duration_{$i}_days"] ?? 0),
+                        'hours' => absint($_POST["duration_{$i}_hours"] ?? 0),
+                        'minutes' => absint($_POST["duration_{$i}_minutes"] ?? 0),
+                    ],
+                    'role_active' => $role_active,
+                    'role_expired' => $role_expired,
+                ];
+            }
+        }
+        // Сохраняем массив подписок в опцию
+        update_option('subscription_plans', $all_subscriptions);
+        // Перенаправляем пользователя обратно на страницу настроек с уведомлением об успешном сохранении
+        wp_redirect(admin_url('admin.php?page=subscription-settings&settings-updated=true'));
         exit;
     }
-
-    update_option('subscription_role_active', $role_active);
-    update_option('subscription_role_expired', $role_expired);
-
-    // Формируем массив всех подписок
-    $all_subscriptions = [];
-    for ($i = 1; $i <= 4; $i++) {
-        $product_id = absint($_POST["product_{$i}"] ?? 0);
-
-        if ($product_id > 0) {
-            $all_subscriptions[] = [
-                'product_id' => $product_id,
-                'duration' => [
-                    'years' => absint($_POST["duration_{$i}_years"] ?? 0),
-                    'months' => absint($_POST["duration_{$i}_months"] ?? 0),
-                    'days' => absint($_POST["duration_{$i}_days"] ?? 0),
-                    'hours' => absint($_POST["duration_{$i}_hours"] ?? 0),
-                    'minutes' => absint($_POST["duration_{$i}_minutes"] ?? 0),
-                ],
-                'role_active' => $role_active,
-                'role_expired' => $role_expired,
-            ];
-        }
-    }
-
-    // Сохраняем массив подписок в опцию
-    update_option('subscription_plans', $all_subscriptions);
-
-    // Перенаправляем пользователя обратно на страницу настроек с уведомлением об успешном сохранении
-    wp_redirect(admin_url('admin.php?page=subscription-settings&settings-updated=true'));
-    exit;
-}
 
     /**
      * Получить список ролей
