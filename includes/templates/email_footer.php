b996e7572aadca673bef6dba0265351aa690873f
Alexandra <109337175+avs-alexandra@users.noreply.github.com>
2025-05-15 19:07:17 +0700

    Create email_footer.php

---
+++ includes/templates/email_footer.php
@@ -0,0 +1,22 @@
+<tr>
+    <td align="center" valign="top">
+        <table border="0" cellpadding="10" cellspacing="0" width="600">
+            <tbody>
+            <tr>
+                <td valign="top" style="padding:0">
+                    <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                        <tbody>
+                        <tr>
+                            <td colspan="2" valign="middle"
+                                style="border:0;color:{text_color};font-family:{font_family};font-size:12px;line-height:125%;padding:0 48px 48px 48px;text-align:center">
+                                <p>{description}</p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </td>
+</tr>
