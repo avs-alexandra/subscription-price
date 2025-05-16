4c8d0975409b9147475e6e6292bbbcb38be7a258
Alexandra <109337175+avs-alexandra@users.noreply.github.com>
2025-05-16 16:44:27 +0700

    Create email_header.php

---
+++ templates/email_header.php
@@ -0,0 +1,11 @@
+<tr>
+    <td align="center" valign="top">
+        <table border="0" cellpadding="0" cellspacing="0" width="600" style="background-color:{woocommerce_email_base_color};border-radius:3px 3px 0 0;color:#ffffff;">
+            <tr>
+                <td style="padding:36px 48px;font-family:{font_family};font-size:30px;font-weight:300;line-height:150%;color:#ffffff;text-align:left;">
+                    {title}
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
