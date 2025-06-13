3e722165dacdba48413bbc38ae0018b1a04aed7b
Alexandra <109337175+avs-alexandra@users.noreply.github.com>
2025-06-13 21:19:25 +0700

    Update email_footer.php

--- templates/email_footer.php
+++ templates/email_footer.php
@@ -2,7 +2,7 @@
     <td align="center" valign="top">
         <table border="0" cellpadding="10" cellspacing="0" width="600" style="border-top:1px solid #dedede;background-color:#f5f5f5;">
             <tr>
-                <td style="padding:0 48px 48px 48px;font-family:{font_family};font-size:12px;line-height:125%;color:{text_color};text-align:center;">
+                <td style="padding:0 48px 48px 48px;font-family:{font_family};font-size:13px;line-height:125%;color:{text_color};text-align:center;">
                     <p>{description}</p>
                     <p>&copy; {year} <a href="{site_url}" style="color:{text_color};text-decoration:none;">{site_title}</a></p>
                 </td>
